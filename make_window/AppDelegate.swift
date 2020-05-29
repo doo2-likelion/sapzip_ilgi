c655edc1e89951aee3fac64eb6f3a0bd9a2d9188
Duhee Lee <doo2@likelion.net>
2020-05-29 16:21:30 +0900

    Initial Commit

---
+++ make_window/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  make_window
+//
+//  Created by Duhee Lee on 2020/05/29.
+//  Copyright © 2020 Duhee Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ make_window/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  make_window
+//
+//  Created by Duhee Lee on 2020/05/29.
+//  Copyright © 2020 Duhee Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
